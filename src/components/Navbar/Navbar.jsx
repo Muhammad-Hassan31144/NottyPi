c141732f8363c3d5e16ab3030da5b0a6c8a473b8
Muhammad-Hassan31144 <31144@students.riphah.edu.pk>
2024-07-09 14:47:24 +0500

    Added Async Functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,60 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { RxCross2 } from "react-icons/rx";
+import { fetchUser } from "../../slices/userSlice";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { fetchNotesByContent, getAllNotes } from "../../slices/noteSlice"; // Import the actions
+
 const Navbar = () => {
   const [theme, setTheme] = useState("light");
   const [searchString, setSearchString] = useState("");
+  
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, token } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchUser(token));
+    }
+  }, [token, dispatch]);
+
+  useEffect(() => {
+    if (searchString.trim() === "") {
+      dispatch(getAllNotes());
+    } else {
+      dispatch(fetchNotesByContent(searchString));
+    }
+  }, [searchString, dispatch]);
+
   const handleThemeToggle = () => {
     const newTheme = theme === "light" ? "synthwave" : "light";
     setTheme(newTheme);
     localStorage.setItem("theme", newTheme);
     document.documentElement.setAttribute("data-theme", newTheme);
   };
+
   const handleLogout = () => {
+    dispatch(logout());
     navigate("/login");
   };
 
-  const handleSearch = () => {
-    console.log(searchString);
+  const handleSearchInputChange = (e) => {
+    setSearchString(e.target.value);
   };
 
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") || "light";
-    setTheme(savedTheme);
-    document.documentElement.setAttribute("data-theme", savedTheme);
-  }, []);
+  const clearSearch = () => {
+    setSearchString("");
+  };
+
+  const getInitials = (name) => {
+    if (!name) return "U"; // Default initial
+    const initials = name.split(" ").map(word => word[0].toUpperCase()).join("");
+    return initials;
+  };
+
   return (
     <nav className="navbar bg-base-100">
       <div className="flex-1">
@@ -38,60 +68,27 @@ const Navbar = () => {
               className="grow"
               placeholder="Search"
               value={searchString}
-              onChange={(e) => setSearchString(e.target.value)}
+              onChange={handleSearchInputChange}
             />
-            <FaMagnifyingGlass size={22} className="text-primary cursor-pointer" onClick={handleSearch} />
-
+            <FaMagnifyingGlass size={22} className="text-primary cursor-pointer" />
             {searchString && (
-              <RxCross2 size={22} className="text-primary cursor-pointer" onClick={() => setSearchString("")} />
+              <RxCross2 size={22} className="text-primary cursor-pointer" onClick={clearSearch} />
             )}
           </label>
         </div>
         <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar p-0 rounded-full"
-          >
-            <div className="w-10 rounded-full flex items-center justify-center">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                className="rounded-full w-16 h-16 object-cover"
-              />
-            </div>
+          <div className="w-10 h-10 rounded-full flex items-center justify-center bg-primary text-white">
+            {getInitials(user?.fullName)}
           </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <button className="btn btn-ghost" onClick={handleLogout}>
-                Logout
-              </button>
-            </li>
-          </ul>
         </div>
       </div>
       <label className="swap swap-rotate">
-        {/* this hidden checkbox controls the state */}
         <input
           type="checkbox"
           className="theme-controller"
           checked={theme === "synthwave"}
           onChange={handleThemeToggle}
         />
-
-        {/* sun icon */}
         <svg
           className="swap-off fill-current w-10 h-10"
           xmlns="http://www.w3.org/2000/svg"
@@ -99,8 +96,6 @@ const Navbar = () => {
         >
           <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
         </svg>
-
-        {/* moon icon */}
         <svg
           className="swap-on fill-current w-10 h-10"
           xmlns="http://www.w3.org/2000/svg"
